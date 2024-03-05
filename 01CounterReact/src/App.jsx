6d3dc785f11b8eeb6c7e2ed697aece389542e9e3
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-03-06 00:25:41 +0530

    set counter project done

--- 01CounterReact/src/App.jsx
+++ 01CounterReact/src/App.jsx
@@ -1,13 +1,34 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
+    let [counter, setCounter] = useState(15);
+    const addValue=()=>{
+      console.log('clicked',counter);
+      counter=counter+1;
+      setCounter(counter)
+    }
 
-  return (
-    <>
-      
+    const removeValue=()=>{
+      counter= counter-1;
+      setCounter(counter);
+    }
+    
+  return ( 
+    
+    <>  
       <h1>Chai aur react</h1>
+      <h2>counter Value:{counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value</button>
     </>
   )
 }
