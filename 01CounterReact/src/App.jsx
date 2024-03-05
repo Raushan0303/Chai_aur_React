cfa7f047e12b30cf0828e3d0ec77d88d6a25869a
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-03-06 00:31:49 +0530

    01CountReact project done

--- 01CounterReact/src/App.jsx
+++ 01CounterReact/src/App.jsx
@@ -1,36 +1,36 @@
-
 import { useState } from 'react';
 import './App.css'
 
 function App() {
 
     let [counter, setCounter] = useState(15);
-    const addValue=()=>{
-      console.log('clicked',counter);
-      counter=counter+1;
-      setCounter(counter)
+
+    const addValue = () => {
+        if (counter < 20) {
+            setCounter(counter + 1);
+        }
     }
 
-    const removeValue=()=>{
-      counter= counter-1;
-      setCounter(counter);
+    const removeValue = () => {
+        if (counter > 0) {
+            setCounter(counter - 1);
+        }
     }
-    
-  return ( 
-    
-    <>  
-      <h1>Chai aur react</h1>
-      <h2>counter Value:{counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value</button>
-      <br />
-      <button
-      onClick={removeValue}
-      >remove value</button>
-    </>
-  )
+    return (
+        <>
+            <h1>Chai aur react</h1>
+            <h2>counter Value:{counter}</h2>
+
+            <button
+                onClick={addValue}
+            >Add value</button>
+            <br />
+            <button
+                onClick={removeValue}
+            >remove value</button>
+        </>
+    )
 }
 
-export default App
+export default App;
